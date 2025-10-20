9a2889a34569748466fca86294e8e9225312177b
vsr2414 <visitha2414@gmail.com>
2025-10-20 13:53:45 +0530

    new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/eslint.config.js 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg     new file:   frontend/src/App.jsx 	new file:   frontend/src/assets/add_icon_green.png 	new file:   frontend/src/assets/add_icon_white.png 	new file:   frontend/src/assets/app_store.png 	new file:   frontend/src/assets/assets.js 	new file:   frontend/src/assets/bag_icon.png 	new file:   frontend/src/assets/basket_icon.png     new file:   frontend/src/assets/cross_icon.png 	new file:   frontend/src/assets/facebook_icon.png 	new file:   frontend/src/assets/food_1.png 	new file:   frontend/src/assets/food_10.png 	new file:   frontend/src/assets/food_11.png 	new file:   frontend/src/assets/food_12.png 	new file:   frontend/src/assets/food_13.png 	new file:   frontend/src/assets/food_14.png 	new file:   frontend/src/assets/food_15.png 	new file:   frontend/src/assets/food_16.png 	new file:   frontend/src/assets/food_17.png 	new file:   frontend/src/assets/food_18.png 	new file:   frontend/src/assets/food_19.png 	new file:   frontend/src/assets/food_2.png 	new file:   frontend/src/assets/food_20.png 	new file:   frontend/src/assets/food_21.png 	new file:   frontend/src/assets/food_22.png 	new file:   frontend/src/assets/food_23.png 	new file:   frontend/src/assets/food_24.png 	new file:   frontend/src/assets/food_25.png 	new file:   frontend/src/assets/food_26.png 	new file:   frontend/src/assets/food_27.png 	new file:   frontend/src/assets/food_28.png 	new file:   frontend/src/assets/food_29.png 	new file:   frontend/src/assets/food_3.png 	new file:   frontend/src/assets/food_30.png 	new file:   frontend/src/assets/food_31.png 	new file:   frontend/src/assets/food_32.png 	new file:   frontend/src/assets/food_4.png 	new file:   frontend/src/assets/food_5.png 	new file:   frontend/src/assets/food_6.png 	new file:   frontend/src/assets/food_7.png 	new file:   frontend/src/assets/food_8.png 	new file:   frontend/src/assets/food_9.png 	new file:   frontend/src/assets/header_img.png 	new file:   frontend/src/assets/linkedin_icon.png 	new file:   frontend/src/assets/logo.png 	new file:   frontend/src/assets/logout_icon.png 	new file:   frontend/src/assets/menu_1.png 	new file:   frontend/src/assets/menu_2.png 	new file:   frontend/src/assets/menu_3.png 	new file:   frontend/src/assets/menu_4.png 	new file:   frontend/src/assets/menu_5.png 	new file:   frontend/src/assets/menu_6.png 	new file:   frontend/src/assets/menu_7.png 	new file:   frontend/src/assets/menu_8.png 	new file:   frontend/src/assets/parcel_icon.png 	new file:   frontend/src/assets/play_store.png 	new file:   frontend/src/assets/profile_icon.png 	new file:   frontend/src/assets/rating_starts.png 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/assets/remove_icon_red.png 	new file:   frontend/src/assets/search_icon.png 	new file:   frontend/src/assets/selector_icon.png 	new file:   frontend/src/assets/twitter_icon.png	new file:   frontend/src/components/Navbar/Navbar.css 	new file:   frontend/src/components/Navbar/Navbar.jsx 	new file:   frontend/src/index.css 	new file:   frontend/src/main.jsx 	new file:   frontend/vite.config.js

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
+// ...existing code...ttt
