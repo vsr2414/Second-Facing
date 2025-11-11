5c051c6b0355cedf22a3e6d620e1e56f5bee6e53
vsr2414 <visitha2414@gmail.com>
2025-11-12 00:50:06 +0530

    modified:   backend/.env 	modified:   backend/controllers/orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,8 @@
 import orderModel from "../models/orderModel.js";
 import userModel from '../models/userModel.js'
-import stripe from "stripe"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order for frontend
 const placeOrder = async (req,res) => {
