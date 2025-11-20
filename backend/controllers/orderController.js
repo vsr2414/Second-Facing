ff36a7e3d1b999804623212aa9eb24de1bf0f4e6
vsr2414 <visitha2414@gmail.com>
2025-11-20 21:09:16 +0530

    new file:   backend/.env 	modified:   backend/controllers/orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,6 +7,48 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) => {
 
+    const frontend_url = "http://localhost:5123";
+
+    try {
+        const newOrder = new orderModel ({
+    userId:req.body.userId,
+    items:req.body.items,
+    amount:req.body.amount,
+    address:req.body.address,
+        })
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+     const line_items = req.body.items.map((item)=>({
+        price_data:{
+            currency:"inr",
+            product_data:{
+                name:item.name
+            },
+            unit_amount:item.price*100*80
+        },
+        quantity:item.quantity
+            }))
+
+            line_items.push({
+                price_data:{
+                    currency:"inr",
+                    product_data:{
+                        name:"Delivery Charges"
+                    },
+                    unit_amount:2*100
+                },
+                quantity:1
+            })
+              
+            const session =await stripe.checkout.sessions.create({
+              line_items:line_items,
+              mode: 'payment',
+              success_url:`${frontend_url}/verify?success=true`
+            })
+        
+        }catch (error) {
+
+   }
 }
 
 export {placeOrder}
